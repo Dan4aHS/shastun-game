b07658c70ec3704851fca59d011558ff690f9cd4
dan4a <danshtal@gmail.com>
2024-12-27 23:28:24 +0300

    NoMoves Test added

--- chips/chips.go
+++ chips/chips.go
@@ -1,12 +1,15 @@
 package chips
 
 type Chips struct {
+	current []int
 }
 
 func NewChips() *Chips {
-	return &Chips{}
+	return &Chips{
+		current: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+	}
 }
 
 func (c *Chips) Current() []int {
-	return []int{}
+	return c.current
 }
--- chips/chips_test.go
+++ chips/chips_test.go
@@ -2,11 +2,30 @@ package chips
 
 import "testing"
 
-func TestInit(t *testing.T) {
-	_ = NewChips()
+func TestCurrentChipsWithoutMoves(t *testing.T) {
+	c := NewChips()
+	res := c.Current()
+
+	assertIntSlices(t, res, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
 }
 
-func TestCurrentChips(t *testing.T) {
-	c := NewChips()
-	c.Current()
+func assertIntSlices(t *testing.T, actual, expected []int) {
+	if len(actual) != len(expected) {
+		t.Errorf("actual %v != expected %v", actual, expected)
+	}
+
+	expectedMap := make(map[int]int)
+	for v := range expected {
+		expectedMap[v]++
+	}
+
+	for v := range actual {
+		expectedMap[v]--
+	}
+
+	for _, v := range expectedMap {
+		if v != 0 {
+			t.Errorf("actual %v != expected %v", actual, expected)
+		}
+	}
 }
