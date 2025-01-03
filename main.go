c78210daa88054b13a4ff465003d1203bb72a440
dan4a <danshtal@gmail.com>
2025-01-03 23:15:47 +0300

    added async gaming

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 	"shastun-game/chips"
 	"shastun-game/game"
 	time_rater "shastun-game/time-rater"
+	"sync"
+)
+
+const (
+	runnersCount  = 100
+	runChunkCount = 10000
 )
 
 func main() {
-	runCount := 100000
+	totalGames := runChunkCount * runnersCount
+	fmt.Printf("Total Games: %d\n", totalGames)
 
 	testSum, smartSum := 0, 0
 	testChips := []int{1, 2, 3, 4, 11, 12}
@@ -17,14 +24,46 @@ func main() {
 	tr := time_rater.NewTimeRater(chips.NewChipsFromSlice(testChips))
 	fmt.Printf("TIME_RATER: %f\n\n", tr.RateTime())
 
-	for i := 0; i < runCount; i++ {
-		g := game.NewGame(testChips)
+	partialTestCh := make(chan int, runnersCount)
+	partialSmartCh := make(chan int, runnersCount)
 
-		testRes, smartRes := g.Play()
-		testSum += testRes
-		smartSum += smartRes
+	var wg sync.WaitGroup
+
+	for range runnersCount {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			runAsyncGames(testChips, runChunkCount, partialTestCh, partialSmartCh)
+		}()
 	}
 
-	fmt.Printf("test avg: %f\n", float64(testSum)/float64(runCount))
-	fmt.Printf("smart avg: %f\n", float64(smartSum)/float64(runCount))
+	go func() {
+		wg.Wait()
+		close(partialTestCh)
+		close(partialSmartCh)
+	}()
+
+	for testPartial := range partialTestCh {
+		testSum += testPartial
+	}
+
+	for smartPartial := range partialSmartCh {
+		smartSum += smartPartial
+	}
+
+	fmt.Printf("test avg: %f\n", float64(testSum)/float64(runnersCount*runChunkCount))
+	fmt.Printf("smart avg: %f\n", float64(smartSum)/float64(runnersCount*runChunkCount))
+}
+
+func runAsyncGames(chips []int, count int, testCh, smartCh chan int) {
+	localTestSum, localSmartSum := 0, 0
+	for i := 0; i < count; i++ {
+		g := game.NewGame(chips)
+		testRes, smartRes := g.Play()
+		localTestSum += testRes
+		localSmartSum += smartRes
+	}
+
+	testCh <- localTestSum
+	smartCh <- localSmartSum
 }
